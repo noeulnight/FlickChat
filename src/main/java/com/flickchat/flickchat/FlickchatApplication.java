63cb7f272557a4837a9000ca3065b4de6bd0cbc2
Taehyun Lim <admin@limtaehyun.dev>
2023-04-17 11:50:04 +0900

    create springboot file

---
+++ src/main/java/com/flickchat/flickchat/FlickchatApplication.java
@@ -0,0 +1,13 @@
+package com.flickchat.flickchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlickchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlickchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flickchat/flickchat/FlickchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flickchat.flickchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlickchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
